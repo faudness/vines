b3ff37b293f0f3e7bd2755ef9e9933829209d8d6
MD2K23 <50792101+MD2K23@users.noreply.github.com>
2023-10-26 16:34:48 +0100

    Update hideVineItems.user.js
    Created UK specific version

--- hideVineItems.user.js
+++ hideVineItems.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
-// @name        Hide Vine Items
-// @namespace   https://github.com/robartsd/VineTools/
-// @match       https://www.amazon.com/vine/vine-items*
+// @name        Hide Vine Items UK
+// @namespace   https://github.com/MD2K23/VineToolsUK
+// @match       https://www.amazon.co.uk/vine/vine-items*
+// @exclude     https://www.amazon.co.uk/vine/vine-items*search=*
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
@@ -11,21 +12,12 @@
 // ==/UserScript==
 
 var hiddenCount = 0;
-
-{
-  var messageSpan = document.createElement("span");
-  messageSpan.classList.add("hideVineItems-message");
-  messageSpan.innerHTML = ` - <a id="hideVineItems-togglePage">hide <span id="hideVineItems-count">${hiddenCount}</span> items</a>, <a id="hideVineItems-hideAll">hide all (this page)</a>`;
-  messageSpan.querySelector("#hideVineItems-togglePage").addEventListener("click", (e) => {
-    document.querySelector(":root").classList.toggle("hideVineItems-showHidden");
-  });
-  messageSpan.querySelector("#hideVineItems-hideAll").addEventListener("click", (e) => {
-    document.querySelectorAll(".vvp-item-tile:not(.hideVineItems-hideASIN) .hideVineItems-toggleASIN").forEach( (hideLink) => {
-      hideLink.click();
-    })
-  });
-  document.querySelector("#vvp-items-grid-container > p").append(messageSpan);
-}
+var messageSpan = document.createElement("span");
+messageSpan.classList.add("hideVineItems-message");
+messageSpan.innerHTML = ` - <a id="hideVineItems-togglePage">Hide/Unhide items (<span id="hideVineItems-count">${hiddenCount}</span> hidden)</a><a id="hideVineItems-hideAll">Hide all items on this page</a>`;
+messageSpan.querySelector("#hideVineItems-togglePage").addEventListener("click", (e) => {document.querySelector(":root").classList.toggle("hideVineItems-showHidden");})
+messageSpan.querySelector("#hideVineItems-hideAll").addEventListener("click", (e) => {document.querySelectorAll(".vvp-item-tile:not(.hideVineItems-hideASIN) .hideVineItems-toggleASIN").forEach( (hideLink) => {hideLink.click();})});
+document.querySelector("#vvp-items-grid-container > p").append(messageSpan);
 
 function updateCount() {
   document.getElementById("hideVineItems-count").innerHTML = `${hiddenCount}`;
@@ -39,7 +31,7 @@ function addHideLink(tile, ASIN) {
   var tileContent = tile.querySelector(".vvp-item-tile-content");
   if (tileContent) {
     var a = document.createElement("a");
-    a.innerHTML = "hide item";
+    a.innerHTML = "🚫";
     a.addEventListener("click", (e) => {
       tile.classList.toggle("hideVineItems-hideASIN");
       if (isHidden(ASIN)) {
@@ -74,25 +66,35 @@ GM_addStyle(`
 .hideVineItems-hideASIN {
   display:none;
 }
+
 .vvp-item-tile-content {
   position: relative;
 }
+
 .hideVineItems-toggleASIN {
   position: absolute;
-  width: auto !important;
-  top: 0;
-  right: 0;
-  background-color: #3339;
+  width: 25px !important;
+  height: 20px !important;
+  overflow: hidden;
+  top: 7px;
+  right: 1px;
+  background-color: rgba(0,0,0,0.0);
   padding: 0 .5em;
 }
+
 .hideVineItems-hideASIN .hideVineItems-toggleASIN::before,
-:root:not(.hideVineItems-showHidden) #hideVineItems-togglePage::before
+:root:not(.hideVineItems-showHidden)
 {
-  content: "un";
+  content: "🧿";
   display: inline;
 }
+
 .hideVineItems-showHidden .hideVineItems-hideASIN {
   display:unset;
   opacity: 50%
+}
+
+#hideVineItems-hideAll {
+ margin-left:20px;
 }
 `);
