6c36e7b9f1b1b3b2b8ac0cab3cc7d9d96aafc189
MD2K23 <50792101+MD2K23@users.noreply.github.com>
2023-11-07 14:22:01 +0000

    Update hideVineItems.user.js

--- hideVineItems.user.js
+++ hideVineItems.user.js
@@ -3,20 +3,44 @@
 // @namespace   https://github.com/MD2K23/VineToolsUK
 // @match       https://www.amazon.co.uk/vine/vine-items*
 // @exclude     https://www.amazon.co.uk/vine/vine-items*search=*
+// @match       https://www.amazon.com/vine/vine-items*
+// @exclude     https://www.amazon.com/vine/vine-items*search=*
+// @match       https://www.amazon.ca/vine/vine-items*
+// @exclude     https://www.amazon.ca/vine/vine-items*search=*
+// @match       https://www.amazon.fr/vine/vine-items*
+// @exclude     https://www.amazon.fr/vine/vine-items*search=*
+// @match       https://www.amazon.de/vine/vine-items*
+// @exclude     https://www.amazon.de/vine/vine-items*search=*
+// @match       https://www.amazon.it/vine/vine-items*
+// @exclude     https://www.amazon.it/vine/vine-items*search=*
+// @match       https://www.amazon.es/vine/vine-items*
+// @exclude     https://www.amazon.se/vine/vine-items*search=*
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
 // @grant       GM_addStyle
-// @version     1.1
+// @version     1.2
 // @description Adds a hide button to items offered in Amazon Vine. Fork of script in VineTools: https://github.com/robartsd/VineTools by robartsd: https://github.com/robartsd
 // ==/UserScript==
+
+var hideSymbol="🚫"
+var unhideSymbol="🧿"
+
+//English Language support
+if (location.hostname == "www.amazon.co.uk" || location.hostname == "www.amazon.com"){
+    var hiddenText="Hidden"
+    var showMessage="Show hidden items"
+    var hideMessage="Hide all items on this page"
+    var unhideMessage="Unhide all items on this page"
+}
 
 var hiddenCount = 0;
 var messageSpan = document.createElement("span");
 messageSpan.classList.add("hideVineItems-message");
-messageSpan.innerHTML = ` - <a id="hideVineItems-togglePage">Hide/Unhide items (<span id="hideVineItems-count">${hiddenCount}</span> hidden)</a><a id="hideVineItems-hideAll">Hide all items on this page</a>`;
+messageSpan.innerHTML = ` (<span id="hideVineItems-count">(${hiddenCount}</span><span style="margin-right:10px"> ${hiddenText})</span>&#x2022<span id="hideVineItems-toggleText">${showMessage}</span><label class="switch"><input id="hideVineItems-togglePage" type="checkbox"><span class="slider round"></span></label>&#x2022<a id="hideVineItems-hideAll">${hideSymbol} ${hideMessage}</a>&#x2022<a id="hideVineItems-unhideAll">${unhideSymbol} ${unhideMessage}</a>`;
 messageSpan.querySelector("#hideVineItems-togglePage").addEventListener("click", (e) => {document.querySelector(":root").classList.toggle("hideVineItems-showHidden");})
 messageSpan.querySelector("#hideVineItems-hideAll").addEventListener("click", (e) => {document.querySelectorAll(".vvp-item-tile:not(.hideVineItems-hideASIN) .hideVineItems-toggleASIN").forEach( (hideLink) => {hideLink.click();})});
+messageSpan.querySelector("#hideVineItems-unhideAll").addEventListener("click", (e) => {document.querySelectorAll(".vvp-item-tile .hideVineItems-toggleASIN").forEach( (hideLink) => {hideLink.click();})});
 document.querySelector("#vvp-items-grid-container > p").append(messageSpan);
 
 function updateCount() {
@@ -31,7 +55,7 @@ function addHideLink(tile, ASIN) {
   var tileContent = tile.querySelector(".vvp-item-tile-content");
   if (tileContent) {
     var a = document.createElement("a");
-    a.innerHTML = "🚫";
+    a.innerHTML = hideSymbol;
     a.addEventListener("click", (e) => {
       tile.classList.toggle("hideVineItems-hideASIN");
       if (isHidden(ASIN)) {
@@ -85,7 +109,7 @@ GM_addStyle(`
 .hideVineItems-hideASIN .hideVineItems-toggleASIN::before,
 :root:not(.hideVineItems-showHidden)
 {
-  content: "🧿";
+  content: "${unhideSymbol}";
   display: inline;
 }
 
@@ -94,7 +118,70 @@ GM_addStyle(`
   opacity: 50%
 }
 
-#hideVineItems-hideAll {
- margin-left:20px;
+#hideVineItems-hideAll, #hideVineItems-unhideAll, #hideVineItems-togglePage, #hideVineItems-toggleText {
+ margin-left:10px;
+ margin-right:10px;
 }
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 32px;
+  height: 20px;
+  margin-right:10px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 12px;
+  width: 12px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(12px);
+  -ms-transform: translateX(12px);
+  transform: translateX(12px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 12px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
 `);
