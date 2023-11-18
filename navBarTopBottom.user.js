592189a5b54497c718fa770a1c129ba5baa92be7
MD2K23 <50792101+MD2K23@users.noreply.github.com>
2023-11-18 00:27:26 +0000

    Update navBarTopBottom.user.js

--- navBarTopBottom.user.js
+++ navBarTopBottom.user.js
@@ -9,13 +9,18 @@
 // @match       https://www.amazon.it/vine/vine-items*
 // @match       https://www.amazon.es/vine/vine-items*
 // @grant       GM_addStyle
-// @version     1.0
+// @version     1.0.1
 // @description Displays the pagination nav bar at both the top and bottom on the Amazon Vine store
 // ==/UserScript==
 
+var products=document.querySelectorAll(".vvp-item-tile")
+var hiddenproducts=document.querySelectorAll(".vvp-item-tile.hideVineItems-hideASIN")
+var filteredproducts=document.querySelectorAll(".vvp-item-tile.hideVineItems-filterProduct")
+var difference=(products.length - hiddenproducts.length - filteredproducts.length)
+
 // Add nav bar to top
 var navBar=document.querySelector('#vvp-items-grid-container > div.a-text-center');
-if (navBar != null){
+if (navBar != null && difference > 0){
     var topNav=document.createElement("div");
     topNav.classList.add("a-text-center");
     topNav.innerHTML = navBar.innerHTML;
