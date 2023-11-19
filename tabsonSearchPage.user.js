69e3e893049a9d5bbb9c18df944b6c8ec6891fe2
MD2K23 <50792101+MD2K23@users.noreply.github.com>
2023-11-19 22:27:30 +0000

    Update tabsonSearchPage.user.js

--- tabsonSearchPage.user.js
+++ tabsonSearchPage.user.js
@@ -8,13 +8,13 @@
 // @match       https://www.amazon.de/vine/vine-items*
 // @match       https://www.amazon.it/vine/vine-items*
 // @match       https://www.amazon.es/vine/vine-items*
-// @version     1.0.2
+// @version     1.0.3
 // @description Adds the RFY, AFA amnd AI tabs to the Search page on the Amazon Vine store
 // ==/UserScript==
 
 // Add RFY, AFA, AI bar to search page
 if ((location.search).includes("search=")){
-    var testDiv= document.createElement("div");
-    testDiv.innerHTML=`<span id="vvp-items-button--recommended" class="a-button a-button-normal a-button-toggle" role="radio"><span class="a-button-inner"><a href="vine-items?queue=potluck" class="a-button-text">Recommended for you</a></span></span><span id="vvp-items-button--all" class="a-button a-button-toggle" role="radio" aria-checked="true"><span class="a-button-inner"><a href="vine-items?queue=last_chance" class="a-button-text">Available for all</a></span></span><span id="vvp-items-button--seller" class="a-button a-button-normal a-button-toggle" role="radio"><span class="a-button-inner"><a href="vine-items?queue=encore" class="a-button-text">Additional items</a></span></span>`
-    document.querySelector("#vvp-items-button-container").append(testDiv);
+    var tabBar= document.createElement("span");
+    tabBar.innerHTML=`<span id="vvp-items-button--recommended" class="a-button a-button-normal a-button-toggle" role="radio"><span class="a-button-inner"><a href="vine-items?queue=potluck" class="a-button-text">Recommended for you</a></span></span><span id="vvp-items-button--all" class="a-button a-button-toggle" role="radio" aria-checked="true"><span class="a-button-inner"><a href="vine-items?queue=last_chance" class="a-button-text">Available for all</a></span></span><span id="vvp-items-button--seller" class="a-button a-button-normal a-button-toggle" role="radio"><span class="a-button-inner"><a href="vine-items?queue=encore" class="a-button-text">Additional items</a></span></span>`
+    document.querySelector("#vvp-items-button-container").append(tabBar);
 }
