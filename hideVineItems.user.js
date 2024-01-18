dc7cdfa14d53e4e703b4812c9daa925aba1288ef
faudness <156624733+faudness@users.noreply.github.com>
2024-01-17 19:57:27 -0600

    Update hideVineItems.user.js

--- hideVineItems.user.js
+++ hideVineItems.user.js
@@ -8,8 +8,11 @@
 // @grant       GM_deleteValue
 // @grant       GM_addStyle
 // @grant       GM_listValues
-// @version     1.2
+// @version     1.3
 // @description Vines
+// @homepageURL https://github.com/faudness/Vines
+// @updateURL https://raw.githubusercontent.com/faudness/Vines/master/hideVineItems.user.js
+// @downloadURL https://raw.githubusercontent.com/faudness/Vines/master/hideVineItems.user.js
 // ==/UserScript==
 
 var A = [
